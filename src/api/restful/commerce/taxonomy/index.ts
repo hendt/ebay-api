6ea2458457a4f63109275687af235656da5fd73a
Nick Bordeau <nickbordeau@gmail.com>
2021-02-21 14:17:13 -0500

    fix: udpate comment and return type to more accuratly decribe what is returned from eBay

--- src/api/restful/commerce/taxonomy/index.ts
+++ src/api/restful/commerce/taxonomy/index.ts
@@ -127,9 +127,11 @@ export default class Taxonomy extends Api {
     /**
      * This call returns a complete list of aspects for all of the leaf categories that belong to an eBay marketplace.
      *
-     *  @param categoryTreeId
+     * @param categoryTreeId
+     *
+     * @return A JSON GZIP compressed file buffer
      */
-    public fetchItemAspects(categoryTreeId: string) {
+    public fetchItemAspects(categoryTreeId: string): Promise<Buffer> {
         const cId = encodeURIComponent(categoryTreeId);
         return this.get(`/category_tree/${cId}/fetch_item_aspects`, {
             responseType: 'arraybuffer'
