533d94c340e8c631c5a41db5277c712f24b02df0
dantio <daniil.tomilow@gmail.com>
2021-09-12 12:39:05 +0200

    chore(deps): bump axios from 0.21.1 to 0.21.4

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import {terser} from "rollup-plugin-terser";
 import strip from '@rollup/plugin-strip';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'lib/index.js',
@@ -19,6 +20,7 @@ export default {
     resolve({
       browser: true
     }),
+    json(),
     commonjs(),
     terser({
       keep_fnames: true
