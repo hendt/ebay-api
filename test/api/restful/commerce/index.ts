c80f1afd59d2f3cd534ff6976f779f7574676da3
dantio <daniil.tomilow@gmail.com>
2022-01-05 10:00:27 +0100

    fix: commerce.translations oas test

--- test/api/restful/commerce/index.ts
+++ test/api/restful/commerce/index.ts
@@ -2,7 +2,7 @@ import {Catalog, Identity, Taxonomy, Translation, Charity, Notification} from '.
 import CatalogOas from './catalog/commerce_catalog_v1_beta_oas3.json';
 import IdentityOas from './identity/buy_identity_v1_oas3.json';
 import TaxonomyOas from './taxonomy/commerce_taxonomy_v1_oas3.json';
-import TranslationOas from './translation/commerce_translation_v1_oas3.json';
+import TranslationOas from './translation/commerce_translation_v1_beta_oas3.json';
 import CharityOas from './charity/commerce_charity_v1_oas3.json';
 import NotificationOas from './notification/commerce_notification_v1_oas3.json';
 
