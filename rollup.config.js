eb3dae0cfb35a238ef3998ca0882f205e186678d
Lukas Jaronis <jaronisluke13@gmail.com>
2024-05-22 09:19:09 -0700

    Add alias

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
-import terser from '@rollup/plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import bundleSize from 'rollup-plugin-bundle-size';
-import virtual from '@rollup/plugin-virtual';
+import terser from "@rollup/plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import bundleSize from "rollup-plugin-bundle-size";
+import virtual from "@rollup/plugin-virtual";
+import alias from "@rollup/plugin-alias";
 
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 const plugins = [
   virtual({
@@ -13,36 +14,47 @@ const plugins = [
   }),
   bundleSize(),
   resolve({
-    browser: true
+    browser: true,
   }),
   json(),
   commonjs(),
   terser({
-    keep_fnames: true
+    keep_fnames: true,
   }),
-]
+  alias({
+    entries: [
+      {
+        find: "crypto",
+        replacement: "node:crypto",
+      },
+    ],
+  }),
+];
 
-export default [{
-  input: './lib/index.js',
-  output: [
-    {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'eBayApi',
-      exports: 'default',
-      sourcemap: false,
-    },
-  ],
-  plugins
-}, {
-  input: './dist/eBayApi.js',
-  output: [
-    {
-      file: pkg['browser:esm'],
-      format: 'esm',
-      exports: 'named'
-    },
-  ],
-  context: 'window',
-  plugins
-}]
+export default [
+  {
+    input: "./lib/index.js",
+    output: [
+      {
+        file: pkg.browser,
+        format: "umd",
+        name: "eBayApi",
+        exports: "default",
+        sourcemap: false,
+      },
+    ],
+    plugins,
+  },
+  {
+    input: "./dist/eBayApi.js",
+    output: [
+      {
+        file: pkg["browser:esm"],
+        format: "esm",
+        exports: "named",
+      },
+    ],
+    context: "window",
+    plugins,
+  },
+];
