d78ba0a0c0b89712f68468a3c4e5cd278b2d4ad7
Jonas Brenner <jones@freshx.de>
2024-06-20 14:42:43 +0200

    Add get_hazardous_materials_labels endpoint

--- src/api/restful/sell/metadata/index.ts
+++ src/api/restful/sell/metadata/index.ts
@@ -132,4 +132,19 @@ export default class Metadata extends Restful implements OpenApi<operations> {
       }
     });
   }
+
+
+  /**
+   * This method returns hazardous materials label information for the specified eBay marketplace. The information
+   * includes IDs, descriptions, and URLs (as applicable) for the available signal words, statements, and pictograms.
+   * The returned statements are localized for the default langauge of the marketplace. 
+   * If a marketplace does not support hazardous materials label information, an error is returned.
+   *
+   * @param marketplaceId This path parameter specifies the eBay marketplace for which hazardous material labels are
+   *     retrieved.
+   */
+  public getHazardousMaterialsLabels(marketplaceId: string) {
+    marketplaceId = encodeURIComponent(marketplaceId);
+    return this.get(`/marketplace/${marketplaceId}/get_hazardous_materials_labels`);
+  }
 }
