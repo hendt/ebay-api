a596838569b940025bf11b335f170b7adcb96c66
Jonas Brenner <jones@freshx.de>
2024-06-20 14:48:54 +0200

    Remove trailing whitespace

--- src/api/restful/sell/metadata/index.ts
+++ src/api/restful/sell/metadata/index.ts
@@ -137,7 +137,7 @@ export default class Metadata extends Restful implements OpenApi<operations> {
   /**
    * This method returns hazardous materials label information for the specified eBay marketplace. The information
    * includes IDs, descriptions, and URLs (as applicable) for the available signal words, statements, and pictograms.
-   * The returned statements are localized for the default langauge of the marketplace. 
+   * The returned statements are localized for the default langauge of the marketplace.
    * If a marketplace does not support hazardous materials label information, an error is returned.
    *
    * @param marketplaceId This path parameter specifies the eBay marketplace for which hazardous material labels are
